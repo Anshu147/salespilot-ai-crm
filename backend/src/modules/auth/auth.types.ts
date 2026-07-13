32a50742a3c67d7f2817cbd455b5a84fa2e357b7
Anshu147 <anshu902956@gmail.com>
2026-07-13 21:16:59 +0530

    Login Feature added

--- backend/src/modules/auth/auth.types.ts
+++ backend/src/modules/auth/auth.types.ts
@@ -1,7 +1,14 @@
 import type { User } from "@prisma/client";
-
+import type { mapUserToDTO } from "../../utils/user.mapper.js";
 export interface RegisterResponse {
     user: Omit<User, "password">;
     accessToken: string;
     refreshToken: string;
 }
+
+export interface LoginResponse {
+    user: ReturnType<typeof mapUserToDTO>;
+    accessToken: string;
+    refreshToken: string;
+}
+
